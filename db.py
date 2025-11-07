f521f211b8c4e332492166f234e36154bf279397
Naved Abbasi <navedabbasi91@gmail.com>
2025-11-07 11:05:42 +0530

    Update database password in get_connection function

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ def get_connection():
     return pymysql.connect(
         host='127.0.0.1',
         user='root',
-        password='Password',
+        password='333333333',
         database='music_store',
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=False
